0bcaa87f1f47c63933255e521bd45244fc448023
Dan Hooper <dan.c.hooper@gmail.com>
2020-10-22 10:50:01 -0400

    Added some mythtv tweaks

--- mythtv/README.md
+++ mythtv/README.md
@@ -1,6 +1,11 @@
 ## Myth Frontend Restart
-`ln -s mythfrontend.desktop ~/.config/autostart/`
-`ln -s mythrestart.sh ~/`
+`ln -s $(pwd)/mythfrontend.desktop ~/.config/autostart/`
+`ln -s $(pwd)/mythrestart.sh ~/`
 
 ## Media Center Remote
-`ln -s rc6_mce /etc/rc_keymaps/`
+`sudo  mkdir -p /etc/rc_keymaps`
+`sudo ln -s $(pwd)/rc6_mce /etc/rc_keymaps/`
+`ln -s $(pwd)/keyboards.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/keyboards.xml`
+
+## Software
+`sudo apt install ir-keytable`
--- mythtv/mythrestart.sh
+++ mythtv/mythrestart.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # mythrestart.sh
 # Automatically restart mythfrontend if it fails.
-
+ir-keytable -D 5000 -P 5000
 xset -dpms
 xset s off
-xset r rate 1000 30
+xset s noblank
+xset r rate 700 255
 # Loop the call of mythfrontend.
 while [ /bin/true ]
 do
         killall mythfrontend
-        sleep 5 && xset r rate 1000 30
-        mythfrontend -v playback >> /home/dhooper/mythfrontend.log
+        sleep 5 && xset r rate 700 255
+	ir-keytable -D 5000 -P 5000
+	xset -dpms
+	xset s off
+	xset s noblank
+        # mythfrontend -v playback >> /home/dhooper/mythfrontend.log
+        mythfrontend >> /home/dhooper/mythfrontend.log
         sleep 2
 done
